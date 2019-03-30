9ae93e2bfd3ea447d44e8d2886cb990c72448fc4
iwangyantao <iwangyantao@163.com>
2019-03-30 14:27:35 +0800

    commit
    Signed-off-by: iwangyantao <iwangyantao@163.com>

---
+++ Example/Pods/Target Support Files/YTNetWork/YTNetWork-umbrella.h
@@ -0,0 +1,22 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ATListResTools.h"
+#import "ATNetParamsTools.h"
+#import "BaseListYTKRequest.h"
+#import "BaseYTKRequest.h"
+#import "CCNetResModel.h"
+#import "MSApi.h"
+
+FOUNDATION_EXPORT double YTNetWorkVersionNumber;
+FOUNDATION_EXPORT const unsigned char YTNetWorkVersionString[];
+
